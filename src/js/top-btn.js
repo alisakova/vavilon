602e37be2296807b77b456b175c62a11ae081c2c
alisakova <coctail_rossini@mail.ru>
2019-02-24 16:02:36 +0300

    vending machines page

--- src/js/top-btn.js
+++ src/js/top-btn.js
@@ -1,25 +1,3 @@
-
-$(document).ready(function(){
-  if ($('._slider').length) {
-    $('._slider').slick({
-      infinite: true,
-      speed: 500,
-      autoplay: true,
-      autoplaySpeed: 15000,
-      fade: true,
-      arrows: false,
-      dots: true,
-    });
-  }
-  if ($('.companies-slider').length) {
-    $('.companies-slider').slick({
-      slidesToShow: 5,
-      infinite: true,
-      // adaptiveHeight: true
-    });
-  }
-});
-
 var toTopBtn = document.querySelector(".top-btn");
 var scrollAnimation;
 
