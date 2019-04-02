5060c800c2f9809c27da828bf10c64e000c01abc
alisakova <coctail_rossini@mail.ru>
2019-04-02 11:46:23 +0300

    update

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -3,13 +3,13 @@ var infoItems = document.querySelectorAll(".product-card__add-info-item");
 if (infoItems) {
   infoItems.forEach(function(item) {
     item.addEventListener('click', function() {
+      var tooltip = item.querySelector('.tooltip');
       infoItems.forEach(function(infoItem) {
         var tooltip1 = infoItem.querySelector('.tooltip');
-        if (tooltip1.classList.contains('active')) {
+        if (tooltip1.classList.contains('active') && tooltip1 !== tooltip) {
           tooltip1.classList.remove('active');
         }
       });
-      var tooltip = item.querySelector('.tooltip');
       if (window.innerWidth < 768) {
         tooltip.classList.toggle('active');
       }
